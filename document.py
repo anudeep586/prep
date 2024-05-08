8aa57c2b5cb60885e9e54b696815136fae01ef4d
L N M Anudeep <61861542+anudeep586@users.noreply.github.com>
2024-05-08 17:25:41 +0530

    Update document.py

--- document.py
+++ document.py
@@ -4,8 +4,6 @@ class Solution:
         if amount==0:
             return 0
         dic={}
-        for i in coins:
-            dic[i]=1
         def coin(amount):
             if amount==0:
                 return 0
@@ -14,12 +12,13 @@ class Solution:
             k=float('inf')
             for i in coins:
                 if i<=amount:
-                    k=min(k,1+coin(amount-i)) #instead of p we are doing something like 1+ which means increasing len(arr)
+                    k=min(k,1+coin(amount-i))# instead of p we are doing something like 1+ to know the path
             dic[amount]=k
             return dic[amount]
         self.result=coin(amount)
         if self.result==float('inf'):
             return -1
         return self.result
+#when we are trying something like this first we should do like line 7 and line 8 thinking about the ground case and initialzing
 
 # for memoization use something like k=float("inf") when we are doing something like for i in coins(recursion)
